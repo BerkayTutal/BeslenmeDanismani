14e02948107079e5419f226ce01aa9104babcf09
BerkayTutal <berkay.tutal@hotmail.com>
2017-05-15 16:25:10 +0300

    First commit To use Navbar and nav drawer extend from BaseDrawerActivity anything you put in Constraintslayout extended from BaseDrawerActivity will be left behind of toolbar

---
+++ app/src/main/java/tr/com/berkaytutal/beslenmedanismani/LoginActivity.java
@@ -0,0 +1,16 @@
+package tr.com.berkaytutal.beslenmedanismani;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_login);
+        Intent i = new Intent(this,HomepageActivity.class);
+        startActivity(i);
+    }
+}
